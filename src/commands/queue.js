9d38d3e29ef0cbcf04e6d1fbd60237f3383d5e8c
max-debinsky <maksdeb.kontakt@gmail.com>
2024-04-16 19:34:20 +0200

    queue.js implement

---
+++ src/commands/queue.js
@@ -0,0 +1,31 @@
+const {SlashCommandBuilder} = require("@discordjs/builders");
+const {MessageEmbed} = require("discord.js");
+const { execute } = require("./play");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName("skip")
+    .setDescription("Skips the current song."),
+    execute: async ({client, interaction}) => {
+        const queue = client.player.getQueue(interaction.guild);
+
+        if(!queue || !queue.playing) {
+            await interaction.reply("There is no song playing.");
+            return;
+        }
+
+        const queueString = queue.tracks.slice(0, 10).map((song, i) => {
+            return `${i + 1})  [${song.duration}]\` ${song.title} - <@${song.requrestedBy.id}>`;
+        }).join("\n");
+
+        const currentSong = queue.current;
+
+        await interaction.reply({
+            embeds: [
+                new MessageEmbed()
+                .setDescription(`**Currently Playing: **\n\` ${currentSong.title} - <@${currentSong.requrestedBy.id}> \n\n**Queue:**\n${queueString}`)
+                .setThumbnail(currentSong.thumbnail)
+            ]
+        })
+    }
+}
