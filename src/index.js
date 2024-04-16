1a18d9e60b17ee5bb770a0cab1709835626353b9
max-debinsky <maksdeb.kontakt@gmail.com>
2024-04-16 18:47:45 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,33 @@
+require("dotenv").config();
+
+const {REST} = require("@discordjs/rest");
+const {Routes} = require("discord-api-types");
+const {Client, Intents, Collection} = require("discord.js");
+const {Player} = require("discord-player");
+
+const fs = require("node:fs");
+const path = require("node:path");
+
+const client = new Client({
+    intents: [
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MESSAGES,
+        Intents.FLAGS.GUILD_VOICE_STATES
+    ]
+});
+
+// Command loader
+const commands = [];
+client.commands = new Collection();
+
+const commandsPath = path.join(__dirname, "commands");
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith(".js"));
+
+for(const file of commandFiles) {
+    const filePath = path.join(__dirname, "commands");
+    const command = require(filePath);
+
+    client.commands.set(command.data.name, command);
+    commands.push(command);
+}
+
